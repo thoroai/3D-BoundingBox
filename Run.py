683d4b3930ba0da8758363a7574e74a69b861a7a
Cameron Kisailus <ckisailus@thoro.ai>
2023-10-05 14:33:56 -0400

    remove stuff from fork

--- Run.py
+++ Run.py
@@ -1,201 +1,113 @@
 """
-Images must be in ./Kitti/testing/image_2/ and camera matricies in ./Kitti/testing/calib/
-
-Uses YOLO to obtain 2D box, PyTorch to get 3D box, plots both
-
-SPACE bar for next image, any other key to exit
+This script will use the 2D box from the label rather than from YOLO,
+but will still use the neural nets to get the 3D position and plot onto the
+image. Press space for next image and escape to quit
 """
-
-
 from torch_lib.Dataset import *
 from library.Math import *
 from library.Plotting import *
 from torch_lib import Model, ClassAverages
-from yolo.yolo import cv_Yolo
 
 import os
-import time
-
-import numpy as np
 import cv2
+import time
 
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from torchvision.models import vgg
+import numpy as np
 
-import argparse
+# to run car by car
+single_car = False
 
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument("--image-dir", default="eval/image_2/",
-                    help="Relative path to the directory containing images to detect. Default \
-                    is eval/image_2/")
-
-# TODO: support multiple cal matrix input types
-parser.add_argument("--cal-dir", default="camera_cal/",
-                    help="Relative path to the directory containing camera calibration form KITTI. \
-                    Default is camera_cal/")
-
-parser.add_argument("--video", action="store_true",
-                    help="Weather or not to advance frame-by-frame as fast as possible. \
-                    By default, this will pull images from ./eval/video")
-
-parser.add_argument("--show-yolo", action="store_true",
-                    help="Show the 2D BoundingBox detecions on a separate image")
-
-parser.add_argument("--hide-debug", action="store_true",
-                    help="Supress the printing of each 3d location")
-
-
-def plot_regressed_3d_bbox(img, cam_to_img, box_2d, dimensions, alpha, theta_ray, img_2d=None):
+def plot_regressed_3d_bbox(img, truth_img, cam_to_img, box_2d, dimensions, alpha, theta_ray):
 
     # the math! returns X, the corners used for constraint
     location, X = calc_location(dimensions, cam_to_img, box_2d, alpha, theta_ray)
 
     orient = alpha + theta_ray
 
-    if img_2d is not None:
-        plot_2d_box(img_2d, box_2d)
-
+    plot_2d_box(truth_img, box_2d)
     plot_3d_box(img, cam_to_img, orient, dimensions, location) # 3d boxes
 
     return location
 
 def main():
 
-    FLAGS = parser.parse_args()
-
-    # load torch
     weights_path = os.path.abspath(os.path.dirname(__file__)) + '/weights'
     model_lst = [x for x in sorted(os.listdir(weights_path)) if x.endswith('.pkl')]
     if len(model_lst) == 0:
         print('No previous model found, please train first!')
         exit()
     else:
-        print('Using previous model %s'%model_lst[-1])
+        print ('Using previous model %s'%model_lst[-1])
         my_vgg = vgg.vgg19_bn(pretrained=True)
-        # TODO: load bins from file or something
         model = Model.Model(features=my_vgg.features, bins=2).cuda()
         checkpoint = torch.load(weights_path + '/%s'%model_lst[-1])
         model.load_state_dict(checkpoint['model_state_dict'])
         model.eval()
 
-    # load yolo
-    yolo_path = os.path.abspath(os.path.dirname(__file__)) + '/weights'
-    yolo = cv_Yolo(yolo_path)
-
+    # defaults to /eval
+    dataset = Dataset(os.path.abspath(os.path.dirname(__file__)) + '/eval')
     averages = ClassAverages.ClassAverages()
 
-    # TODO: clean up how this is done. flag?
-    angle_bins = generate_bins(2)
-
-    image_dir = FLAGS.image_dir
-    cal_dir = FLAGS.cal_dir
-    if FLAGS.video:
-        if FLAGS.image_dir == "eval/image_2/" and FLAGS.cal_dir == "camera_cal/":
-            image_dir = "eval/video/2011_09_26/image_2/"
-            cal_dir = "eval/video/2011_09_26/"
-
-
-    img_path = os.path.abspath(os.path.dirname(__file__)) + "/" + image_dir
-    # using P_rect from global calibration file
-    calib_path = os.path.abspath(os.path.dirname(__file__)) + "/" + cal_dir
-    calib_file = calib_path + "calib_cam_to_cam.txt"
-
-    # using P from each frame
-    # calib_path = os.path.abspath(os.path.dirname(__file__)) + '/Kitti/testing/calib/'
-
-    try:
-        ids = [x.split('.')[0] for x in sorted(os.listdir(img_path))]
-    except:
-        print("\nError: no images in %s"%img_path)
-        exit()
-
-    for img_id in ids:
+    all_images = dataset.all_objects()
+    for key in sorted(all_images.keys()):
 
         start_time = time.time()
 
-        img_file = img_path + img_id + ".png"
+        data = all_images[key]
 
-        # P for each frame
-        # calib_file = calib_path + id + ".txt"
-
-        truth_img = cv2.imread(img_file)
+        truth_img = data['Image']
         img = np.copy(truth_img)
-        yolo_img = np.copy(truth_img)
+        objects = data['Objects']
+        cam_to_img = data['Calib']
 
-        detections = yolo.detect(yolo_img)
-
-        for detection in detections:
-
-            if not averages.recognized_class(detection.detected_class):
-                continue
-
-            # this is throwing when the 2d bbox is invalid
-            # TODO: better check
-            try:
-                detectedObject = DetectedObject(img, detection.detected_class, detection.box_2d, calib_file)
-            except:
-                continue
-
+        for detectedObject in objects:
+            label = detectedObject.label
             theta_ray = detectedObject.theta_ray
             input_img = detectedObject.img
-            proj_matrix = detectedObject.proj_matrix
-            box_2d = detection.box_2d
-            detected_class = detection.detected_class
 
             input_tensor = torch.zeros([1,3,224,224]).cuda()
             input_tensor[0,:,:,:] = input_img
+            input_tensor.cuda()
 
             [orient, conf, dim] = model(input_tensor)
             orient = orient.cpu().data.numpy()[0, :, :]
             conf = conf.cpu().data.numpy()[0, :]
             dim = dim.cpu().data.numpy()[0, :]
 
-            dim += averages.get_item(detected_class)
+            dim += averages.get_item(label['Class'])
 
             argmax = np.argmax(conf)
             orient = orient[argmax, :]
             cos = orient[0]
             sin = orient[1]
             alpha = np.arctan2(sin, cos)
-            alpha += angle_bins[argmax]
+            alpha += dataset.angle_bins[argmax]
             alpha -= np.pi
 
-            if FLAGS.show_yolo:
-                location = plot_regressed_3d_bbox(img, proj_matrix, box_2d, dim, alpha, theta_ray, truth_img)
-            else:
-                location = plot_regressed_3d_bbox(img, proj_matrix, box_2d, dim, alpha, theta_ray)
+            location = plot_regressed_3d_bbox(img, truth_img, cam_to_img, label['Box_2D'], dim, alpha, theta_ray)
 
-            if not FLAGS.hide_debug:
-                print('Estimated pose: %s'%location)
-
-        if FLAGS.show_yolo:
-            numpy_vertical = np.concatenate((truth_img, img), axis=0)
-            cv2.imshow('SPACE for next image, any other key to exit', numpy_vertical)
-        else:
-            cv2.imshow('3D detections', img)
-
-        if not FLAGS.hide_debug:
-            print("\n")
-            print('Got %s poses in %.3f seconds'%(len(detections), time.time() - start_time))
+            print('Estimated pose: %s'%location)
+            print('Truth pose: %s'%label['Location'])
             print('-------------')
 
-        if FLAGS.video:
-            cv2.waitKey(1)
-        else:
-            if cv2.waitKey(0) != 32: # space bar
-                exit()
+            # plot car by car
+            if single_car:
+                numpy_vertical = np.concatenate((truth_img, img), axis=0)
+                cv2.imshow('2D detection on top, 3D prediction on bottom', numpy_vertical)
+                cv2.waitKey(0)
+
+        print('Got %s poses in %.3f seconds\n'%(len(objects), time.time() - start_time))
+
+        # plot image by image
+        if not single_car:
+            numpy_vertical = np.concatenate((truth_img, img), axis=0)
+            cv2.imshow('2D detection on top, 3D prediction on bottom', numpy_vertical)
+            if cv2.waitKey(0) == 27:
+                return
 
 if __name__ == '__main__':
     main()
